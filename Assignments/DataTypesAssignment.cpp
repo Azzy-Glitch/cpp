826e3521574962769234cbc82a668f65a6dd2d71
Abdul Aziz <aapppp742@gmail.com>
2024-12-24 14:07:45 +0500

    Update DataTypesAssignment.cpp

--- Assignments/DataTypesAssignment.cpp
+++ Assignments/DataTypesAssignment.cpp
@@ -32,7 +32,7 @@ int main()
     isStudent = (isStudentInput == 'y' || isStudentInput == 'Y');
 
     // Output the values of all variables
-    cout << "\n\t\t\t-------- Initial Information --------" << endl;
+    cout << "\n\t\t-------- Initial Information --------" << endl;
     cout << "\n\t\tFull Name: " << fullName << endl;
     cout << "\t\tYour Age: " << age << endl;
     cout << "\t\tYour Height: " << fixed << setprecision(2) << height << " meters" << endl; // Format height to 2 decimal places
@@ -41,12 +41,12 @@ int main()
 
     // Add 5 years to age and output the new value
     age += 5;
-    cout << "\n\t\t\t-------- After 5 Years --------" << endl;
+    cout << "\n\t\t-------- After 5 Years --------" << endl;
     cout << "\n\t\tUpdated Age: " << age << endl;
 
     // Modify isStudent and print the updated value
     isStudent = false; // Set to false
-    cout << "\n\t\t\t-------- Student Status Change --------" << endl;
+    cout << "\n\t\t-------- Student Status Change --------" << endl;
     cout << "\n\t\tIs Student: " << isStudent << endl;
 
     // Declare a constant PI and calculate the area of a circle
@@ -54,7 +54,7 @@ int main()
     double radius = 5.0;
     double area = PI * radius * radius;
 
-    cout << "\n\t\t\t-------- Circle Calculation --------" << endl;
+    cout << "\n\t\t-------- Circle Calculation --------" << endl;
     cout << "\n\t\tArea of a circle with radius " << radius << " is: " << fixed << setprecision(2) << area << endl;
 
     cout << endl;
